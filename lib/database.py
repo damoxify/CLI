b44ae05ba0c9a161c58ff5b55e9c4417c0dd1220
Adedamola Ajasa <azeemajasa@gmail.com>
2023-10-23 08:26:54 +0100

    database

--- lib/database.py
+++ lib/database.py
@@ -1,5 +1,5 @@
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-# engine = create_engine('sqlite:///my_database.db')
-# Session = sessionmaker(bind=engine)
+engine = create_engine('sqlite:///my_database.db')
+Session = sessionmaker(bind=engine)
